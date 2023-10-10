20525053a1ac4b0718c00924417d8c241450c336
dimipet <dimi.petridis@gmail.com>
2023-10-10 14:32:42 +0300

    differenciate database user from postgres user, localize variables in functions, simplify pg_ready check, remove sudo -u postgres from checks - now checking with database user, refatcors

--- nextcloud/nextcloud-mover.sh
+++ nextcloud/nextcloud-mover.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
-
 # -----------------------------------------------------------------------------
 # DEFAULT APP SETTINGS
+
+# postgres server settings
+src_pg_user=postgres
+dst_pg_user=postgres
 
 # postgres source DB settings
 src_db_host=localhost
@@ -44,7 +46,7 @@ ftp_user="change-this-to-your-username"
 ftp_password="change-this-to-your-password"
 ftp_remote_dir="/"
 
-# DEFAULT APP SETTINGS END HERE 
+# DEFAULT APP SETTINGS END HERE
 # -----------------------------------------------------------------------------
 
 # get ISO 8601 timestamp
@@ -84,25 +86,40 @@ echo_usage() {
 }
 
 ftp_upload() {
+    local l_ftp_protocol=$1
+    local l_ftp_host=$2
+    local l_ftp_port=$3
+    local l_ftp_user=$4
+    local l_ftp_password=$5
+    local l_ftp_remote_dir=$6
+    local l_ftp_file=$7
+
     lftp -c open -e "\
 	set ftps:initial-prot; \
 	set ftp:ssl-force true; \
 	set ftp:ssl-protect-data true; \
 	set ssl:verify-certificate false; \
-	put -O $ftp_remote_dir $1; \
+	put -O $l_ftp_remote_dir $l_ftp_file; \
    " \
-        -u "$ftp_user","$ftp_password" "$ftp_protocol"://"$ftp_host":"$ftp_port"
+        -u "$l_ftp_user","$l_ftp_password" "$l_ftp_protocol"://"$l_ftp_host":"$l_ftp_port"
 }
 
 ftp_list() {
+    local l_ftp_protocol=$1
+    local l_ftp_host=$2
+    local l_ftp_port=$3
+    local l_ftp_user=$4
+    local l_ftp_password=$5
+    local l_ftp_remote_dir=$6
+
     lftp -c open -e "\
 	set ftps:initial-prot; \
 	set ftp:ssl-force true; \
 	set ftp:ssl-protect-data true; \
 	set ssl:verify-certificate false; \
-	ls -la $ftp_remote_dir; \
+	ls -la $l_ftp_remote_dir; \
    " \
-        -u "$ftp_user","$ftp_password" "$ftp_protocol"://"$ftp_host":"$ftp_port"
+        -u "$l_ftp_user","$l_ftp_password" "$l_ftp_protocol"://"$l_ftp_host":"$l_ftp_port"
 }
 
 exit_bad() {
@@ -110,94 +127,113 @@ exit_bad() {
     exit -1
 }
 
-# FIXME use for src and dst 
-# FIXME accept host/port/user/password as parameters for 
 check_postgres() {
-    if command -v pg_dump &> /dev/null ; then
+    local l_pg_user=$1
+    local l_db_host=$2
+    local l_db_port=$3
+    local l_db_name=$4
+    local l_db_user=$5
+    local l_db_password=$6
+
+    if command -v pg_dump &>/dev/null; then
         echo "checks      : postgres pg_dump found" | tee -a "$log"
-    else 
+    else
         echo "checks      : pg_dump could not be found - are you sure you are running postgres in this host ?" | tee -a "$log"
         exit_bad
     fi
 
-    if lsof -Pi :"$src_db_port" -sTCP:LISTEN -t >/dev/null ; then
-        echo "checks      : postgres listening on port $src_db_port" | tee -a "$log"
-    else 
-        echo "checks      : postgres not listening port $src_db_port" | tee -a "$log"
+    if lsof -Pi :"$l_db_port" -sTCP:LISTEN -t >/dev/null; then
+        echo "checks      : postgres listening on port $l_db_port" | tee -a "$log"
+    else
+        echo "checks      : postgres not listening port $l_db_port" | tee -a "$log"
         exit_bad
     fi
 
-    # FIXME add user + password here
-    pg_isready -d "$src_db_name" -h "$src_db_host" -p "$src_db_port" -U "$src_db_user" | grep 'accepting connections' &> /dev/null
+    # make sure specific ip/host and specific port is accepting connections
+    pg_isready -h "$l_db_host" -p "$l_db_port" | grep 'accepting connections' &>/dev/null
     if [ $? -eq 0 ]; then
         echo "checks      : postgres accepting connections" | tee -a "$log"
-    else 
+    else
         echo "checks      : postgres not accepting connections" | tee -a "$log"
         exit_bad
     fi
 
-    # FIXME add -h host here + user + password
-    sudo -u "$src_db_user" psql -p "$src_db_port" -lqt | cut -d \| -f 1 | grep -qw nextcloud
+    # make sure that the specific database user can connect to specific host:port/database
+    psql postgresql://"$l_db_user":"$l_db_password"@"$l_db_host":"$l_db_port"/"$l_db_name" -lqt | cut -d \| -f 1 | grep -qw "$l_db_name"
     if [ $? -eq 0 ]; then
-        echo "checks      : postgres database $src_db_name exists" | tee -a "$log"
-    else 
-        echo "checks      : postgres database $src_db_name does not exists" | tee -a "$log"
+        echo "checks      : postgres database $l_db_user exists" | tee -a "$log"
+        echo "checks      : postgres database $l_db_user can connect to $l_db_host:$l_db_port/$l_db_name" | tee -a "$log"
+        echo "checks      : postgres database $l_db_name exists" | tee -a "$log"
+    else
+        echo "checks      : postgres something of the following went wrong :" | tee -a "$log"
+        echo "checks      : postgres database $l_db_user does not exist OR ..." | tee -a "$log"
+        echo "checks      : postgres database $l_db_user can not connect to $l_db_host:$l_db_port OR ..." | tee -a "$log"
+        echo "checks      : postgres database $l_db_name does not exist" | tee -a "$log"
         exit_bad
     fi
 
 }
 
 check_nextcloud() {
-    if [ -d "$src_nextcloud_path" ]; then
-        echo "checks      : nextcloud $src_nextcloud_path exists" | tee -a "$log"
+    local l_apache_user=$1
+    local l_nextcloud_path=$2
+
+    if [ -d "$l_nextcloud_path" ]; then
+        echo "checks      : nextcloud $l_nextcloud_path exists" | tee -a "$log"
     else
-        echo "checks      : nextcloud $src_nextcloud_path does not exists" | tee -a "$log"
+        echo "checks      : nextcloud $l_nextcloud_path does not exists" | tee -a "$log"
         exit_bad
     fi
 
-    sudo -u "$src_apache_user" php "$src_nextcloud_path"/occ status | grep 'installed: true'&> /dev/null
+    sudo -u "$l_apache_user" php "$l_nextcloud_path"/occ status | grep 'installed: true' &>/dev/null
     if [ $? -eq 0 ]; then
-        echo "checks      : nextcloud $src_nextcloud_path/occ exists" | tee -a "$log"
-    else 
-        echo "checks      : nextcloud $src_nextcloud_path/occ does not exist" | tee -a "$log"
+        echo "checks      : nextcloud $l_nextcloud_path/occ exists" | tee -a "$log"
+    else
+        echo "checks      : nextcloud $l_nextcloud_path/occ does not exist" | tee -a "$log"
         exit_bad
     fi
 
-    sudo -u "$src_apache_user" php "$src_nextcloud_path"/occ maintenance:mode | grep -E 'enabled|disabled'&> /dev/null
+    sudo -u "$l_apache_user" php "$l_nextcloud_path"/occ maintenance:mode | grep -E 'enabled|disabled' &>/dev/null
     if [ $? -eq 0 ]; then
-        echo "checks      : nextcloud $(sudo -u "$src_apache_user" php "$src_nextcloud_path"/occ maintenance:mode)" | tee -a "$log"
-    else 
+        echo "checks      : nextcloud $(sudo -u "$l_apache_user" php "$l_nextcloud_path"/occ maintenance:mode)" | tee -a "$log"
+    else
         echo "checks      : nextcloud maintenance:mode problem" | tee -a "$log"
         exit_bad
     fi
 }
 
 check_ftp() {
-    if command -v lftp &> /dev/null ; then
+    local l_ftp_protocol=$1
+    local l_ftp_host=$2
+    local l_ftp_port=$3
+    local l_ftp_user=$4
+    local l_ftp_password=$5
+    local l_ftp_remote_dir=$6
+
+    if command -v lftp &>/dev/null; then
         echo "checks      : ftp lftp found" | tee -a "$log"
     else
         echo "checks      : ftp lftp could not be found - please install it" | tee -a "$log"
         exit_bad
     fi
 
-    timeout 3 bash -c "cat < /dev/null > /dev/tcp/$ftp_host/$ftp_port"
+    timeout 3 bash -c "cat < /dev/null > /dev/tcp/$l_ftp_host/$l_ftp_port"
     if [ $? -eq 0 ]; then
-        echo "checks      : ftp remote host $ftp_host is accepting requests on port $ftp_port" | tee -a "$log"
-    else 
-        echo "checks      : ftp remote host $ftp_host is not accepting requests on port $ftp_port" | tee -a "$log"
+        echo "checks      : ftp remote host $l_ftp_host is accepting requests on port $l_ftp_port" | tee -a "$log"
+    else
+        echo "checks      : ftp remote host $l_ftp_host is not accepting requests on port $l_ftp_port" | tee -a "$log"
         exit_bad
     fi
 
-    ftp_list &> /dev/null
+    ftp_list $l_ftp_protocol $l_ftp_host $l_ftp_port $l_ftp_user $l_ftp_password $l_ftp_remote_dir &>/dev/null
     if [ $? -eq 0 ]; then
         echo "checks      : ftp can connect to remote host $ftp_host on port $ftp_port" | tee -a "$log"
-    else 
+    else
         echo "checks      : ftp cannot connect to remote host $ftp_host on port $ftp_port" | tee -a "$log"
         exit_bad
     fi
 
 }
-
 
 backup() {
     create_log
@@ -206,10 +242,10 @@ backup() {
     uname -ar | tee -a "$log"
     echo '-------------------------------------------------------------------------' | tee -a "$log"
 
-    # application properties check if arg exist 
+    # application properties check if arg exist
     if [ -z "$1" ]; then
         echo "props       : no argument for external file supplied" | tee -a "$log"
-        echo "props       : using DEFAULT APP SETTINGS from this script" | tee -a "$log"    
+        echo "props       : using DEFAULT APP SETTINGS from this script" | tee -a "$log"
     else
         app_props=$1
         echo "props file  : $app_props" | tee -a "$log"
@@ -226,9 +262,9 @@ backup() {
 
     # check settings sanity
     echo "checks      : starting to check settings sanity" | tee -a "$log"
-    check_postgres
-    check_nextcloud
-    check_ftp    
+    check_postgres "$src_pg_user" "$src_db_host" "$src_db_port" "$src_db_name" "$src_db_user" "$src_db_password"
+    check_nextcloud "$src_apache_user" "$src_nextcloud_path"
+    check_ftp "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir"
     echo '-------------------------------------------------------------------------' | tee -a "$log"
 
     total_time=0
@@ -243,7 +279,7 @@ backup() {
 
     local_start="$(date +%s)"
     echo '-------------------------------------------------------------------------' | tee -a "$log"
-    
+
     # set maintenance:mode
     echo "exec: occ maintenance:mode --on" | tee -a "$log"
     sudo -u www-data php "$src_nextcloud_path"/occ maintenance:mode --on 2>&1 | tee -a "$log"
@@ -258,15 +294,15 @@ backup() {
     echo "exec: postgres pg_dump" | tee -a "$log"
     echo "exec: pg_dump sql output is redirected to $timestamp.$src_pg_dump_filename" | tee -a "$log"
     echo "exec: pg_dump error output is redirected to $log" | tee -a "$log"
-    sudo -u $src_db_user pg_dump --port="$src_db_port" --dbname="$src_db_name" > "$timestamp"."$src_pg_dump_filename" 2>> "$log"
+    sudo -u $src_db_user pg_dump --port="$src_db_port" --dbname="$src_db_name" >"$timestamp"."$src_pg_dump_filename" 2>>"$log"
     # FIXME use tee
     # sudo -u $src_db_user pg_dump --port="$src_db_port" --dbname="$src_db_name" 2>&1 | tee -a "$timestamp"."$src_pg_dump_filename" > /dev/null
     # sudo -u $src_db_user pg_dump --port="$src_db_port" --dbname="$src_db_name" > >(tee -a "$timestamp"."$src_pg_dump_filename") 2> >(tee -a "$log" >&2)
     echo "exec: sha512 hashing to" "$timestamp"."$src_pg_dump_filename_sha512" | tee -a "$log"
-    sha512sum "$timestamp"."$src_pg_dump_filename" | tee "$timestamp"."$src_pg_dump_filename_sha512" > /dev/null
+    sha512sum "$timestamp"."$src_pg_dump_filename" | tee "$timestamp"."$src_pg_dump_filename_sha512" >/dev/null
     echo "exec: uploading ..." | tee -a "$log"
-    ftp_upload "$timestamp"."$src_pg_dump_filename"
-    ftp_upload "$timestamp"."$src_pg_dump_filename_sha512"
+    ftp_upload "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir" "$timestamp"."$src_pg_dump_filename"
+    ftp_upload "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir" "$timestamp"."$src_pg_dump_filename_sha512"
     local_end="$(date +%s)"
     local_exec_time="$((local_end - local_start))"
     total_time="$((total_time + local_exec_time))"
@@ -278,8 +314,8 @@ backup() {
     echo "exec: tar -czvf nextcloud files + sha512 hashing" 2>&1 | tee -a "$log"
     tar -czvf "$timestamp"."$src_nextcloud_backup_file" "$src_nextcloud_path" 2>&1 | tee -a "$log"
     sha512sum "$timestamp"."$src_nextcloud_backup_file" | tee "$timestamp"."$src_nextcloud_backup_file_sha512"
-    ftp_upload "$timestamp"."$src_nextcloud_backup_file"
-    ftp_upload "$timestamp"."$src_nextcloud_backup_file_sha512"
+    ftp_upload "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir" "$timestamp"."$src_nextcloud_backup_file"
+    ftp_upload "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir" "$timestamp"."$src_nextcloud_backup_file_sha512"
     local_end="$(date +%s)"
     local_exec_time="$((local_end - local_start))"
     total_time="$((total_time + local_exec_time))"
@@ -297,18 +333,19 @@ backup() {
     echo '-------------------------------------------------------------------------' | tee -a "$log"
 
     echo "total execution time: $total_time seconds" | tee -a "$log"
-    ftp_upload "$log"
+    ftp_upload "$ftp_protocol" "$ftp_host" "$ftp_port" "$ftp_user" "$ftp_password" "$ftp_remote_dir" "$log"
 
 }
 
 # parse arguments
-while getopts p:a: flag
-do
+while getopts p:a: flag; do
     case "${flag}" in
-        p) properties=${OPTARG};;
-        a) action=${OPTARG};;
-        *) echo_usage >&2
-       exit 1 ;;
+    p) properties=${OPTARG} ;;
+    a) action=${OPTARG} ;;
+    *)
+        echo_usage >&2
+        exit 1
+        ;;
     esac
 done
 
@@ -320,4 +357,3 @@ elif [ "$action" = "restore" ]; then
 else
     echo_usage
 fi
-
