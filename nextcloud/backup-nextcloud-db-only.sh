c8c88e5fa3e0634452822ec62f121899d827539a
dimipet <dimi.petridis@gmail.com>
2023-10-19 13:55:30 +0300

    minor typo fix

--- nextcloud/backup-nextcloud-db-only.sh
+++ nextcloud/backup-nextcloud-db-only.sh
@@ -1,20 +1,20 @@
-þ#!/bin/bash
+#!/bin/bash
 
 echo "Simple backup script"
 echo "Databases, apps, etc and scp them to specific host"
 
 # get ISO 8601 timestamp
-TIMESTAMP=$(date +"%Y%m%dT%H%M%SZ")
+timestamp=$(date +"%Y%m%dT%H%M%SZ")
 
 # stop postgresql 10 and backup database data directory
 systemctl stop postgresql
 
-tar -zcvf $TIMESTAMP.postgres.10.data-directory.tar.gz /var/lib/postgresql/10/main
+tar -zcvf "$timestamp".postgres.10.data-directory.tar.gz /var/lib/postgresql/10/main
 
 # pgdump important dbs
 # nextcloud
 echo "pg_dump db:nextcloud"
-pg_dump -U database_user -W -F t nextcloud > $TIMESTAMP.nextcloud.db.tar
+pg_dump -U database_user -W -F t nextcloud > "$timestamp".nextcloud.db.tar
 
 systemctl start postgresql
 
