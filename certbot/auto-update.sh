dfc64820ce4e1664eb4a9670db81ab19f322b6df
dimipet <dimi.petridis@gmail.com>
2025-03-07 13:48:38 +0200

    updated auto-update certbot script

--- certbot/auto-update.sh
+++ certbot/auto-update.sh
@@ -6,15 +6,27 @@
 #
 ####################################################
 
+# defaults
+pre_script="/usr/bin/true"
+post_script="/usr/bin/true"
+
 # get ISO 8601 timestamp
 TIMESTAMP=$(date +"%Y%m%dT%H%M%SZ")
 
 # open file for output / delete previous contents
 echo "Starting at : $TIMESTAMP" >/var/log/certbot.log 2>&1
 
-/usr/sbin/ufw allow 80 >>/var/log/certbot.log 2>&1
-certbot renew >>/var/log/certbot.log 2>&1
-/usr/sbin/ufw delete allow 80 >>/var/log/certbot.log 2>&1
+# check if param file was supplied
+if [ -n "$2" ] && [ -f "$2" ]; then
+	app_props="$2"
+	source "$app_props"
+	echo "using pre-script  : $pre_script" >>/var/log/certbot.log 2>&1
+	echo "using post-script : $post_script" >>/var/log/certbot.log 2>&1
+else
+	echo "props       : no argument for external file supplied" >>/var/log/certbot.log 2>&1
+fi
+
+certbot renew --pre-hook "$pre_script" --post-hook "$post_script" --dry-run >>/var/log/certbot.log 2>&1
 
 TIMESTAMP=$(date +"%Y%m%dT%H%M%SZ")
 echo "Ended at : $TIMESTAMP" >>/var/log/certbot.log 2>&1
